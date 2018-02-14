0474a44ea62765e085343bfb8a968ecd6e6fcf54
vivek.kapoor <vivek.kapoor@mobileprogrammingllc.com>
2018-02-15 04:39:28 +0530

    made changes... in app delegate

--- AgumentedDemo/AgumentedDemo/AppDelegate.swift
+++ AgumentedDemo/AgumentedDemo/AppDelegate.swift
@@ -9,29 +9,34 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder {
 
     var window: UIWindow?
+    static var shared:AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
 
+}
 
+extension AppDelegate : UIApplicationDelegate {
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
-     
+        
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
     }
 }
-
